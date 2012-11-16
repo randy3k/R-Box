e6abcc5d18bc37b9822dc43fdccd4000fbe098a7
raymondkww <raymondkww.dev@gmail.com>
2012-11-15 22:04:56 -0800

    Remove useless codes

--- Rsublime.py
+++ Rsublime.py
@@ -4,20 +4,24 @@ import os
 import subprocess
 import string
 
+###############################
+#### Some useful functions ####
+###############################
+
+def clean(str):
+    str = string.replace(str, '\\', '\\\\')
+    str = string.replace(str, '"', '\\"')
+    str = string.replace(str, '\'', '\\\\\'')
+    return str
+
+
 ##################################
 #### change working directory ####
 ##################################
 
 class ChangeDirCommand(sublime_plugin.TextCommand):
-    @staticmethod
-    def clean(str):
-        str = string.replace(str, '\\', '\\\\')
-        str = string.replace(str, '"', '\\"')
-        str = string.replace(str, '\'', '\\\\\'')
-        return str
-
     def run(self, edit):
-        path = self.clean(os.path.dirname(self.view.file_name()))
+        path = clean(os.path.dirname(self.view.file_name()))
         args = ['osascript']
         args.extend(['-e', 'tell app "R64" to cmd "setwd(\'' + path + '\')"\n'])
         subprocess.Popen(args)
@@ -27,13 +31,6 @@ class ChangeDirCommand(sublime_plugin.TextCommand):
 #########################
 
 class SendSelectCommand(sublime_plugin.TextCommand):
-    @staticmethod
-    def clean(str):
-        str = string.replace(str, '\\', '\\\\')
-        str = string.replace(str, '"', '\\"')
-        str = string.replace(str, '\'', '\\\\\'')
-        return str
-
     def run(self, edit):
         str = ''
         for sel in self.view.sel():
@@ -41,10 +38,9 @@ class SendSelectCommand(sublime_plugin.TextCommand):
                 str += self.view.substr(self.view.line(sel)) +'\n'
             else:
                 str += self.view.substr(sel) +'\n'
-        str = self.clean(str)
+        str = clean(str)
         args = ['osascript']
         args.extend(['-e','tell app "R64" to cmd "' + str +'"\n'])
-        print args
         subprocess.Popen(args)
 
 ######################
@@ -52,15 +48,8 @@ class SendSelectCommand(sublime_plugin.TextCommand):
 ######################
 
 class SourceCodeCommand(sublime_plugin.TextCommand):
-    @staticmethod
-    def clean(str):
-        str = string.replace(str, '\\', '\\\\')
-        str = string.replace(str, '"', '\\"')
-        str = string.replace(str, '\'', '\\\\\'')
-        return str
-
     def run(self, edit):
-        path = self.clean(self.view.file_name())
+        path = clean(self.view.file_name())
         args = ['osascript']
         args.extend(['-e', 'tell app "R64" to cmd "source(\'' + path + '\')"\n'])
         subprocess.Popen(args)
