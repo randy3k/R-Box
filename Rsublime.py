2a441b27ec5d9a6637daf790932582e082179b5c
Randy Lai <shingx1@gmail.com>
2012-11-19 19:35:31 -0800

    be more object oriented

--- Rsublime.py
+++ Rsublime.py
@@ -4,48 +4,42 @@ import os
 import subprocess
 import string
 
-###############################
-#### Some useful functions ####
-###############################
+########################
+#### R Common CLass ####
+########################
 
-def clean(str):
-    str = string.replace(str, '\\', '\\\\')
-    str = string.replace(str, '"', '\\"')
-    str = string.replace(str, "'", "'\\''")
-    return str
+class RCommon:
+    settings = sublime.load_settings('Rsublime.sublime-settings')
+    Rapp = settings.get('Rapp')
 
-def rcmd(cmd, Rapp):
-    cmd = clean(cmd)
-    cmd = "osascript -e 'tell application \"" + Rapp + "\" to cmd \"" + cmd + "\"'"
-    subprocess.call(cmd, shell=True)
+    def clean(self, str):
+        str = string.replace(str, '\\', '\\\\')
+        str = string.replace(str, '"', '\\"')
+        str = string.replace(str, "'", "'\\''")
+        return str
+
+    def rcmd(self, cmd):
+        cmd = self.clean(cmd)
+        cmd = "osascript -e 'tell application \"" + self.Rapp + "\" to cmd \"" + cmd + "\"'"
+        subprocess.call(cmd, shell=True)
 
 ##################################
 #### change working directory ####
 ##################################
 
-class ChangeDirCommand(sublime_plugin.TextCommand):
+class ChangeDirCommand(sublime_plugin.TextCommand, RCommon):
     # read settings only when the instance is created
     # it is more efficient then reading settings everytime when "run" is executed
-    def __init__(self, _):    
-        self.settings = sublime.load_settings('Rsublime.sublime-settings')
-        self.Rapp = self.settings.get('Rapp')
-        sublime_plugin.TextCommand.__init__(self, _) 
-
     def run(self, edit):
         path = os.path.dirname(self.view.file_name())
         cmd = "setwd(\"" + string.replace(path, '"', '\\"') + "\")"        
-        rcmd(cmd, self.Rapp)
+        self.rcmd(cmd)
 
 #########################
 #### Send codes to R ####
 #########################
 
-class SendSelectCommand(sublime_plugin.TextCommand):
-    def __init__(self, _):    
-        self.settings = sublime.load_settings('Rsublime.sublime-settings')
-        self.Rapp = self.settings.get('Rapp')
-        sublime_plugin.TextCommand.__init__(self, _)   
-
+class SendSelectCommand(sublime_plugin.TextCommand, RCommon):
     def run(self, edit):
         str = ''
         for sel in self.view.sel():
@@ -53,27 +47,22 @@ class SendSelectCommand(sublime_plugin.TextCommand):
                 str += self.view.substr(self.view.line(sel)) +'\n'
             else:
                 str += self.view.substr(sel) +'\n'
-        rcmd(str, self.Rapp)
+        self.rcmd(str)
 
 ######################
 #### Source codes ####
 ######################
 
-class SourceCodeCommand(sublime_plugin.TextCommand):
-    def __init__(self, _):    
-        self.settings = sublime.load_settings('Rsublime.sublime-settings')
-        self.Rapp = self.settings.get('Rapp')
-        sublime_plugin.TextCommand.__init__(self, _)  
-
+class SourceCodeCommand(sublime_plugin.TextCommand, RCommon):
     def run(self, edit):
         path = self.view.file_name()
         cmd = "source(\"" + string.replace(path, '"', '\\"') + "\")"
-        rcmd(cmd, self.Rapp)
+        self.rcmd(cmd)
 ################################
 #### Send Codes to Terminal ####
 ################################
 
-class SendSelectTerminalCommand(sublime_plugin.TextCommand):
+class SendSelectTerminalCommand(sublime_plugin.TextCommand, RCommon):
        def run(self, edit):
         str = ''
         for sel in self.view.sel():
@@ -81,7 +70,7 @@ class SendSelectTerminalCommand(sublime_plugin.TextCommand):
                 str += self.view.substr(self.view.line(sel)) +'\n'
             else:
                 str += self.view.substr(sel) +'\n'
-        str = clean(str)
+        str = self.clean(str)
         args = ['osascript']
         args.extend(['-e', 'tell app "Terminal" to do script "' + str + '" in front window\n'])
         subprocess.Popen(args)
