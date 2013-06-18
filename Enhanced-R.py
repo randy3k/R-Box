a791cb9984117baba0f6cef21e995b17e384c1ed
Randy Lai <shingx1@gmail.com>
2013-06-18 14:38:02 -0700

    use sublime.platform()

--- Enhanced-R.py
+++ Enhanced-R.py
@@ -15,8 +15,8 @@ def escape_dq(string):
 
 # clean command before sending to R
 def clean(cmd):
-    plat = sublime_plugin.sys.platform
-    if plat == "darwin":
+    plat = ssublime.platform()
+    if plat == "osx":
         cmd = escape_dq(cmd)
     cmd = cmd.rstrip('\n')
     if len(re.findall("\n", cmd)) == 0:
@@ -35,8 +35,8 @@ def get(plat, key, default=None):
 # the main function
 def rcmd(cmd):
     cmd = clean(cmd)
-    plat = sublime_plugin.sys.platform
-    if plat == 'darwin':
+    plat = sublime.platform()
+    if plat == 'osx':
         App = get("osx", "App", "R")
         if re.match('R', App):
             args = ['osascript']
@@ -58,7 +58,7 @@ def rcmd(cmd):
                 args.extend(['-e', apple_script])
                 subprocess.Popen(args)
 
-    elif plat == 'win32':
+    elif plat == 'windows':
         App = App = get("windows", "App", "R64")
         if App == "R64":
             progpath = get("windows", "R64", "1")
@@ -81,7 +81,7 @@ def rcmd(cmd):
         args = [ahk_path, ahk_script_path, progpath, cmd ]
         subprocess.Popen(args)
 
-    elif 'linux' in plat:
+    elif plat == 'linux':
         App = get("linux", "App", "tmux")
         if App == "tmux":
             progpath = get("linux", "tmux", "tmux")
@@ -144,14 +144,14 @@ class RappSwitcher(sublime_plugin.WindowCommand):
         sublime.set_timeout(lambda: self.window.show_quick_panel(options, done), 10)
 
     def run(self):
-        plat = sublime_plugin.sys.platform
-        if plat == 'darwin':
+        plat = sublime.platform()
+        if plat == 'osx':
             self.app_list = ["R", "R64", "Terminal", "iTerm"]
             pop_string = ["For R 3.x.x, R is 64 bit ready", "For R 2.x.x only", "Terminal", "iTerm 2"]
-        elif plat == "win32":
+        elif plat == "windows":
             self.app_list = ["R32", "R64", "Rterm32", "Rterm64"]
             pop_string = ["R i386", "R x64", "Rterm i386", "Rterm x64"]
-        elif "linux" in plat:
+        elif plat == "linux":
             self.app_list = ["tmux", "screen"]
             pop_string = ["tmux", "screen"]
         else:
@@ -162,16 +162,16 @@ class RappSwitcher(sublime_plugin.WindowCommand):
     def on_done(self, action):
         if action==-1: return
         settings = sublime.load_settings(settingsfile)
-        plat = sublime_plugin.sys.platform
-        if plat == 'darwin':
+        plat = sublime.platform()
+        if plat == 'osx':
             plat_settings = settings.get('osx')
             plat_settings['App'] = self.app_list[action]
             settings.set('osx', plat_settings)
-        elif plat == "win32":
+        elif plat == "windows":
             plat_settings = settings.get('windows')
             plat_settings['App'] = self.app_list[action]
             settings.set('windows', plat_settings)
-        elif "linux" in plat:
+        elif plat == "linux":
             plat_settings = settings.get('linux')
             plat_settings['App'] = self.app_list[action]
             settings.set('linux', plat_settings)
