6d1443e71267a0c283cb6a2456fa1589d299494a
Randy Lai <randy.cs.lai@gmail.com>
2014-04-17 18:45:50 -0700

    better add source function

--- misc.py
+++ misc.py
@@ -28,16 +28,32 @@ def listdir(view, dir, base, ext, on_done):
     if base:
         fnames = [f for f in fnames if base.lower() in f.lower()]
 
-    display = [os.curdir, os.pardir]+ [">"+f for f in ls if os.path.isdir(os.path.join(dir, f))] + fnames
+    display = ["[ Create a new file ]", "> "+os.curdir, "> "+os.pardir] + \
+        ["> "+f for f in ls if os.path.isdir(os.path.join(dir, f))] + fnames
 
     def on_action(i):
         if i<0: return
-        elif i<1 or display[i][0] == '>':
+        elif display[i][0] == '>':
             target = display[i][2:] if display[i][0] == '>' else display[i]
             target_dir = os.path.normpath(os.path.join(dir, target))
-            sublime.set_timeout(lambda: listdir(view, target_dir, base, ext, on_done), 1)
+            sublime.set_timeout(lambda: listdir(view, target_dir, base, ext, on_done), 10)
+        elif i == 0:
+            view.window().show_input_panel("File: ", "", on_file, None, None)
         else:
-            target_dir = os.path.normpath(os.path.join(dir, display[i]))
-            on_done(target_dir)
+            target = os.path.normpath(os.path.join(dir, display[i]))
+            on_done(target)
 
-    sublime.set_timeout(lambda: view.window().show_quick_panel(display, on_action), 1)
+    def on_file(s):
+        target = os.path.normpath(os.path.join(dir, s))
+        if os.path.exists(target):
+            sublime.message_dialog("File %s exists." % target)
+            pass
+        else:
+            target_dir = os.path.dirname(target)
+            if not os.path.exists(target_dir): os.mkdir(target_dir)
+            f = open(target, 'w')
+            view.window().open_file(target)
+            on_done(target)
+
+
+    sublime.set_timeout(lambda: view.window().show_quick_panel(display, on_action), 10)
