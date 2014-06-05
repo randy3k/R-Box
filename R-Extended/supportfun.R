f1ff2564b4453ebe4965e86f25b65f04253c3144
Randy Lai <randy.cs.lai@gmail.com>
2014-06-05 00:19:32 -0700

    update support fun.R

--- R-Extended/supportfun.R
+++ R-Extended/supportfun.R
@@ -3,6 +3,16 @@ require(rjson)
 
 library(data.table)
 library(ggplot2)
+
+template = structure(list(
+    begin = "\\b(###)\\s*(\\()", end = "(\\))", name = "meta.function-call.r",
+    contentName = "meta.function-call.arguments.r", comment = "base",
+    patterns = list(structure(list(include = "$self"), .Names = "include")),
+    beginCaptures = structure(list(`2` = structure(list(name = "punctuation.definition.parameters.r"), .Names = "name"), `1` = structure(list(name = "support.function.r"), .Names = "name")), .Names = c("2", "1")),
+    endCaptures = structure(list(`1` = structure(list(name = "punctuation.definition.parameters.r"), .Names = "name")), .Names = "1")
+    ),
+    .Names = c("begin", "end", "name", "contentName", "comment", "patterns", "beginCaptures", "endCaptures")
+)
 filter = function(l){
     ind = grep("^[a-zA-Z\\._]+$", l)
     l[ind]
@@ -11,7 +21,7 @@ getfuns = function(pkg){
     filter(ls(pattern="*", paste0("package:",pkg)))
 }
 getregexp = function(pkg){
-    s = fromJSON(file="supportfun.json")
+    s = template
     content = paste0(sub("\\.","\\\\.",getfuns(pkg)),collapse="|")
     s$begin = paste0("\\b(", content, ")\\s*(\\()")
     s
