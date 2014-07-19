9c03016d10fb7ffdf3cdccda68a1c59d4072aa88
Randy Lai <randy.cs.lai@gmail.com>
2014-07-19 04:05:55 -0700

    use load_binary_resource

--- sendtext.py
+++ sendtext.py
@@ -4,8 +4,46 @@ import os
 import subprocess
 import re
 import time
+import sys
 from .misc import *
 
+
+def update_resource(binname):
+    # from https://github.com/weslly/ColorPicker/blob/master/sublimecp.py=
+    targetdir = os.path.join(sublime.packages_path(), 'User', 'R-Box', 'bin')
+    targetpath = os.path.join(targetdir, binname)
+    respath = 'Packages/R-Box/bin/' + binname
+    pkgpath = os.path.join(sublime.installed_packages_path(), 'R-Box.sublime-package')
+    unpkgpath = os.path.join(sublime.packages_path(), 'R-Box', 'bin', binname)
+
+    if os.path.exists(targetpath):
+        targetinfo = os.stat(targetpath)
+    else:
+        if not os.path.exists(targetdir):
+            os.makedirs(targetdir, 0o755)
+        targetinfo = None
+
+    if os.path.exists(unpkgpath):
+        pkginfo = os.stat(unpkgpath)
+    elif os.path.exists(pkgpath):
+        pkginfo = os.stat(pkgpath)
+    else:
+        return
+
+    if targetinfo == None or targetinfo.st_mtime < pkginfo.st_mtime:
+        data = sublime.load_binary_resource(respath)
+        print("* Updating " + targetpath)
+        with open(targetpath, 'wb') as binfile:
+            binfile.write(data)
+            binfile.close()
+
+    if not os.access(targetpath, os.X_OK):
+        os.chmod(targetpath, 0o755)
+
+def plugin_loaded():
+    if sys.platform == "win32":
+        update_resource("AutoHotkeyU32.exe")
+        update_resource("Rgui.ahk")
 
 # the main function
 class RBoxSendTextCommand(sublime_plugin.TextCommand):
@@ -140,4 +178,3 @@ class RBoxSourceCodeCommand(sublime_plugin.TextCommand):
             return
         cmd = "source(\"" +  escape_dq(fname) + "\")"
         self.view.run_command("r_box_send_text", {"cmd": cmd})
-
