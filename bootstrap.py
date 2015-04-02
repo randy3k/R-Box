02f55dd9b54ab20721cc3235a758ac6507bbc120
Randy Lai <randy.cs.lai@gmail.com>
2015-04-01 21:25:30 -0700

    remove __all__ constant

--- bootstrap.py
+++ bootstrap.py
@@ -2,16 +2,6 @@ import sublime
 
 version = sublime.version()
 
-__all__ = [
-    "RBoxAppSwitchCommand",
-    "RBoxCompletions",
-    "RBoxSendSelectionCommand", "RBoxChangeDirCommand",
-    "RBoxSourceCodeCommand", "RSendSelectCommand",
-    "RBoxSourcePromptCommand",
-    "RBoxStatusListener", "RBoxCleanStatus",
-    "update_resources"
-]
-
 if version >= "3000":
     from .rbox import (
         RBoxAppSwitchCommand,
@@ -34,15 +24,12 @@ else:
     )
 
 
-if sublime.platform() == "windows":
-
-    def plugin_loaded():
+def plugin_loaded():
+    if sublime.platform() == "windows":
         update_resources("AutoHotkeyU32.exe")
         update_resources("Rgui.ahk")
         update_resources("Cmder.ahk")
         update_resources("Cygwin.ahk")
 
-    __all__.append("plugin_loaded")
-
-    if sublime.version() < '3000':
-        plugin_loaded()
+if sublime.version() < '3000':
+    plugin_loaded()
