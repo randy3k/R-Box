923e04eaa9f21b54e2047a738892cdb41c87663f
Randy Lai <randy.cs.lai@gmail.com>
2015-11-20 10:26:47 -0500

    fix 64

--- rbox/send_text.py
+++ rbox/send_text.py
@@ -126,11 +126,11 @@ class TextSender:
 
     def _dispatch_r32_windows(self, cmd):
         cmd = self.clean_cmd(cmd)
-        self.execute_ahk_script("Rgui.ahk", cmd, ["0"])
+        self.execute_ahk_script("Rgui.ahk", cmd, [sget("R32", "0")])
 
     def _dispatch_r64_windows(self, cmd):
         cmd = self.clean_cmd(cmd)
-        self.execute_ahk_script("Rgui.ahk", cmd, ["1"])
+        self.execute_ahk_script("Rgui.ahk", cmd, [sget("R64", "1")])
 
     def _dispatch_rstudio_windows(self, cmd):
         cmd = self.clean_cmd(cmd)
