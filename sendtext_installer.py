72a392071ac2ce45f3c119316b8257f46f9e92e3
Randy Lai <randy.cs.lai@gmail.com>
2016-02-23 04:38:09 -0500

    make it possible to disable sendtext installer message

--- sendtext_installer.py
+++ sendtext_installer.py
@@ -3,8 +3,23 @@ import sys
 
 
 def plugin_loaded():
-    if "SendText+" not in sys.modules and "package_control" in sys.modules:
-        ok = sublime.ok_cancel_dialog("R-Box requires SendText+, install it now?")
-        if ok:
-            sublime.active_window().run_command(
-                "advanced_install_package", {"packages": "SendText+"})
+    rsettings = sublime.load_settings('R-Box.sublime-settings')
+    psettings = sublime.load_settings('Preferences.sublime-settings')
+
+    if not rsettings.get("show_sendtext_installer_message", True):
+        return
+
+    if "SendText+" in psettings.get("ignored_packages", []):
+        return
+
+    if "SendText+" in sys.modules:
+        return
+
+    if "package_control" not in sys.modules:
+        return
+
+    ok = sublime.ok_cancel_dialog(
+        "R-Box: Sending code feature requires \"SendText+\", install it now?")
+    if ok:
+        sublime.active_window().run_command(
+            "advanced_install_package", {"packages": "SendText+"})
