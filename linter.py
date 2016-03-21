a67534c87ca5a9aa57d4849ae83ad09138e1c0bf
Randy Lai <randy.cs.lai@gmail.com>
2016-03-21 10:41:43 -0400

    update linter settings only when not set

--- linter.py
+++ linter.py
@@ -5,6 +5,9 @@ def plugin_loaded():
     s = sublime.load_settings("SublimeLinter.sublime-settings")
     if s.has("user"):
         user = s.get("user")
-        user.get("syntax_map", {}).update({"r extended": "r"})
-        s.set("user", user)
-        sublime.save_settings("SublimeLinter.sublime-settings")
+        syntax_map = user.get("syntax_map", {})
+        if "r extended" not in syntax_map:
+            syntax_map.update({"r extended": "r"})
+            user["syntax_map"] = syntax_map
+            s.set("user", user)
+            sublime.save_settings("SublimeLinter.sublime-settings")
