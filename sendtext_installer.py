8bb15bc921505395e22107029dd05b33be149774
Randy Lai <randy.cs.lai@gmail.com>
2016-06-23 18:52:44 -0400

    do not copy rbox settings to sendtextplus anymore

--- sendtext_installer.py
+++ sendtext_installer.py
@@ -4,7 +4,6 @@ import sys
 
 def plugin_loaded():
     rsettings = sublime.load_settings('R-Box.sublime-settings')
-    ssettings = sublime.load_settings('SendText+.sublime-settings')
     psettings = sublime.load_settings('Preferences.sublime-settings')
 
     if not rsettings.get("show_sendtext_installer_message", True):
@@ -24,9 +23,5 @@ def plugin_loaded():
         "R-Box: \"SendTextPlus\" is missing, "
         "sending code feature requires \"SendTextPlus\". Install it now?")
     if ok:
-        prog = rsettings.get("prog")
-        if prog:
-            ssettings.set("prog", prog)
-            sublime.save_settings('SendText+.sublime-settings')
         sublime.active_window().run_command(
             "advanced_install_package", {"packages": "SendTextPlus"})
