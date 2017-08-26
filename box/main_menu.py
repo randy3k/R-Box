43e663d68746e89fd3a339d555c95edbdd69cfd7
Randy Lai <randy.cs.lai@gmail.com>
2017-08-26 15:11:39 -0400

    improve R-Box main menu

--- box/main_menu.py
+++ box/main_menu.py
@@ -1,17 +1,41 @@
 import sublime
 import sublime_plugin
-import os
 import sys
-import re
+import os
+import threading
 
 
-RE_RPROJ = re.compile(".*\.Rproj$")
+_main_menu_is_visible = [False]
+
+
+def main_menu_is_visible():
+    return _main_menu_is_visible[0]
 
 
 class RBoxMainMenuListener(sublime_plugin.EventListener):
+    _window_is_rproject = []
+    _window_is_not_rproject = []
 
-    def should_show_menu(self, view):
+    def window_is_rproj(self, folder):
+        for f in os.listdir(folder):
+            if f.endswith(".Rproj"):
+                return True
 
+    def is_r_project(self, window):
+        if not window:
+            return False
+        folders = window.folders()
+        if window.id() in self._window_is_rproject:
+            return True
+        elif window.id() not in self._window_is_not_rproject and folders:
+            if self.window_is_rproj(folders[0]):
+                self._window_is_rproject.append(window.id())
+                return True
+            else:
+                self._window_is_not_rproject.append(window.id())
+                return False
+
+    def is_r_file(self, view):
         try:
             pt = view.sel()[0].end()
         except:
@@ -25,46 +49,48 @@ class RBoxMainMenuListener(sublime_plugin.EventListener):
                 "source.c++.rcpp"):
             return True
 
-        if view.window():
-            folders = view.window().folders()
-            if folders:
-                for f in os.listdir(folders[0]):
-                    if RE_RPROJ.match(f):
-                        return True
-
         return False
+
+    def on_activated_async(self, view):
+        if view.settings().get('is_widget'):
+            return
+        if hasattr(self, "timer") and self.timer:
+            self.timer.cancel()
+
+        def set_main_menu():
+
+            menu_path = os.path.join(
+                        sublime.packages_path(),
+                        'User', 'R-Box', 'Main.sublime-menu')
+            menu_dir = os.path.dirname(menu_path)
+
+            if self.is_r_project(view.window()) or self.is_r_file(view):
+
+                if not os.path.exists(menu_dir):
+                    os.makedirs(menu_dir, 0o755)
+
+                if not os.path.exists(menu_path):
+                    data = sublime.load_resource(
+                        "Packages/R-Box/support/R-Box.sublime-menu")
+                    with open(menu_path, 'w') as binfile:
+                        binfile.write(data)
+                        binfile.close()
+                _main_menu_is_visible[0] = True
+            else:
+                if os.path.exists(menu_path):
+                    os.remove(menu_path)
+                _main_menu_is_visible[0] = False
+
+        self.timer = threading.Timer(0.1, set_main_menu)
+        self.timer.start()
 
     def on_query_context(self, view, key, operator, operand, match_all):
         if view.settings().get('is_widget'):
             return
 
         if key == "r_box.main_menu_is_visible":
-            out = self.should_show_menu(view)
+            out = _main_menu_is_visible[0] == operand
             return out if operator == 0 else not out
-
-    def on_activated_async(self, view):
-        if view.settings().get('is_widget'):
-            return
-
-        targetpath = os.path.join(
-            sublime.packages_path(),
-            'User', 'R-Box', 'Main.sublime-menu')
-        targetdir = os.path.dirname(targetpath)
-
-        if self.should_show_menu(view):
-
-            if not os.path.exists(targetdir):
-                os.makedirs(targetdir, 0o755)
-
-            if not os.path.exists(targetpath):
-                data = sublime.load_binary_resource(
-                    "Packages/R-Box/support/R-Box.sublime-menu").decode("utf-8")
-                with open(targetpath, 'w') as binfile:
-                    binfile.write(data)
-                    binfile.close()
-        else:
-            if os.path.exists(targetpath):
-                os.remove(targetpath)
 
 
 class RBoxMainMenuClearWorkspace(sublime_plugin.TextCommand):
