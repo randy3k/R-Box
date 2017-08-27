307f2ce946af4ba184961debe91518dc3a61f942
Randy Lai <randy.cs.lai@gmail.com>
2017-08-27 00:07:31 -0400

    revert back old way to send_code

--- box/render.py
+++ box/render.py
@@ -1,6 +1,4 @@
 import sublime_plugin
-import os
-from .utils import escape_dquote
 
 
 class RBoxRenderRmarkdownCommand(sublime_plugin.TextCommand):
@@ -8,13 +6,8 @@ class RBoxRenderRmarkdownCommand(sublime_plugin.TextCommand):
         return self.view.settings().get("syntax").endswith("R Markdown.sublime-syntax")
 
     def run(self, edit):
-        file_name = self.view.file_name()
-        file_path = os.path.dirname(file_name)
-        cmd = "rmarkdown::render(\"{}\", encoding = \"UTF-8\")".format(escape_dquote(file_name))
-        self.view.window().run_command(
-            "r_box_exec",
-            {"cmd": cmd, "cwd": file_path}
-        )
+        cmd = "rmarkdown::render(\"$file\", encoding = \"UTF-8\")"
+        self.view.run_command("send_code", {"cmd": cmd})
 
 
 class RBoxSweaveRnwCommand(sublime_plugin.TextCommand):
@@ -22,18 +15,10 @@ class RBoxSweaveRnwCommand(sublime_plugin.TextCommand):
         return self.view.settings().get("syntax").endswith("R Sweave.sublime-syntax")
 
     def run(self, edit):
-        file_name = self.view.file_name()
-        file_path = os.path.dirname(file_name)
-        file_base_name = os.path.splitext(file_name)[0]
-        cmd = "Sweave(\"{}\");tools::texi2dvi(\"{}.tex\", pdf = TRUE)".format(
-                    escape_dquote(file_path),
-                    escape_dquote(file_name),
-                    escape_dquote(file_base_name)
-            )
-        self.view.window().run_command(
-            "r_box_exec",
-            {"cmd": cmd, "cwd": file_path}
-        )
+        cmd = ("""setwd(\"$file_path\")\n"""
+               """Sweave(\"$file\")\n"""
+               """tools::texi2dvi(\"$file_base_name.tex\", pdf = TRUE)""")
+        self.view.run_command("send_code", {"cmd": cmd})
 
 
 class RBoxKnitRnwCommand(sublime_plugin.TextCommand):
@@ -41,16 +26,7 @@ class RBoxKnitRnwCommand(sublime_plugin.TextCommand):
         return self.view.settings().get("syntax").endswith("R Sweave.sublime-syntax")
 
     def run(self, edit):
-        file_name = self.view.file_name()
-        file_path = os.path.dirname(file_name)
-        file_base_name = os.path.splitext(file_name)[0]
-        cmd = ("knitr::knit(\"{}\", output=\"{}.tex\");" +
-               "tools::texi2dvi(\"{}.tex\", pdf = TRUE)").format(
-                    escape_dquote(file_name),
-                    escape_dquote(file_base_name),
-                    escape_dquote(file_base_name)
-            )
-        self.view.window().run_command(
-            "r_box_exec",
-            {"cmd": cmd, "cwd": file_path}
-        )
+        cmd = ("""setwd(\"$file_path\")\n"""
+               """knitr::knit(\"$file\", output=\"$file_base_name.tex\")\n"""
+               """tools::texi2dvi(\"$file_base_name.tex\", pdf = TRUE)")""")
+        self.view.run_command("send_code", {"cmd": cmd})
